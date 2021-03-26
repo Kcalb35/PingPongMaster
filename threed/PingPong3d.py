a74d3431d7f5c2e3811d7d288944ef21ccc8e751
grasscube <3180101353@zju.edu.cn>
2021-03-26 14:38:38 +0800

    refactor pingpong3d

--- threed/PingPong3d.py
+++ threed/PingPong3d.py
@@ -1,16 +1,33 @@
 import math
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def reflection(v, theta, phi, decay=0):
+def reflection(v, batv, theta, phi, decay=0.1):
+    """
+    速度反射
+
+    :param v:碰撞前的球速
+    :param batv: bat velocity
+    :param theta: bat theta
+    :param phi: bat phi
+    :param decay: energy decay
+    :return: velocity after reflection
+    """
     e = polar2xyz(theta, phi)
     v_vertical = v.project(e)
-    return v - v_vertical.multiply(1 + math.sqrt(1 - decay))
+    batv_vertical = batv.project(e)
+    eta = math.sqrt(1 - decay)
+    return v - v_vertical.multiply(1 + eta) + batv_vertical.multiply(1 + eta)
 
 
 def polar2xyz(theta, phi):
+    """
+    polar cartesian to xzy cartesian
+    :param theta:
+    :param phi:
+    :return:
+    """
     z = math.cos(phi)
     x = math.sin(phi) * math.cos(theta)
     y = math.sin(phi) * math.sin(theta)
@@ -18,6 +35,18 @@ def polar2xyz(theta, phi):
 
 
 def Point2SurfaceDistance(x, y, z, x0, y0, z0, theta, phi):
+    """
+    point to surface distance
+    :param x:
+    :param y:
+    :param z:
+    :param x0:
+    :param y0:
+    :param z0:
+    :param theta:
+    :param phi:
+    :return:
+    """
     e = polar2xyz(theta, phi)
     p = vector3(x0, y0, z0)
     A = e.x
@@ -28,28 +57,112 @@ def Point2SurfaceDistance(x, y, z, x0, y0, z0, theta, phi):
     return distance
 
 
-def DroppintTime(ball):
+def DroppingTime(ball):
+    """
+    calculate a ball from start drop to desk time
+    :param ball:
+    :return: dropping time
+    """
     return (ball.v.z + math.sqrt(ball.v.z ** 2 + 2 * 9.8 * (ball.z - ball.radius))) / 9.8
 
 
 def DroppingPoint(ball):
-    t = DroppintTime(ball)
+    """
+    calculate dropping point
+    :param ball:
+    :return: x,y
+    """
+    t = DroppingTime(ball)
     return ball.x + t * ball.v.x, ball.y + t * ball.v.y
 
 
-def InterceptPoint3(ball, d=0.1):
-    p = DroppingPoint(ball)
+def DroppingStatus(ball):
+    """
+    getting dropping status
+    :param ball:
+    :return: x,y,z,v
+    """
+    x, y = DroppingPoint(ball)
+    t = DroppingTime(ball)
+    v = vector3(ball.v.x, ball.v.y, ball.v.z)
+    v.z -= 9.8 * t
+    v.z = -v.z * math.sqrt(1 - ball.decay)
+    return x, y, 0.02, v
+
+
+def InterceptPoint3(ball, d):
+    """
+    calculate intercept poing with ball and distance
+    :param ball:
+    :param d: shift distance
+    :return: x,y,z, intercept point vz velocity
+    """
+
+    x, y, z, v_bounced = DroppingStatus(ball)
     vxy = math.sqrt(ball.v.x ** 2 + ball.v.y ** 2)
     dx = d * ball.v.x / vxy
     dy = d * ball.v.y / vxy
-    catch_x = p[0] + dx
-    catch_y = p[1] + dy
+    catch_x = x + dx
+    catch_y = y + dy
 
-    t = DroppintTime(ball)
-    vz_after = (9.8 * t - ball.v.z) * math.sqrt(1 - ball.decay)
-    dt = dx / ball.v.x
-    catch_z = vz_after * dt - 0.5 * 9.8 * dt ** 2 + ball.radius
-    return catch_x, catch_y, catch_z, vz_after - 9.8 * dt
+    dt = d / vxy
+    catch_z = v_bounced.z * dt - 0.5 * 9.8 * dt ** 2 + ball.radius
+    return catch_x, catch_y, catch_z, v_bounced.z - 9.8 * dt
+
+
+def FastForward(ball, bat_v, catch_x, catch_y, catch_z, theta, phi):
+    """
+    从头快速计算碰撞。
+
+    :param bat_v: bat velocity
+    :param ball: ball with decay
+    :param catch_x: bounce x
+    :param catch_y: bounce y
+    :param catch_z: bounce z
+    :param theta: bat theta
+    :param phi: bat phi
+    :return: table bounce success, lat bounce point, last bounce point velocity
+    """
+
+    try:
+        drop_x, drop_y, drop_z, v = DroppingStatus(ball)
+    except Exception as e:
+        return False, None, None
+
+    up_t = math.sqrt((catch_x - ball.x) ** 2 + (catch_y - ball.y) ** 2) / math.sqrt(ball.v.x ** 2 + ball.v.y ** 2)
+    v.z -= 9.8 * up_t
+    v_after = reflection(ball, bat_v, theta, phi, ball.decay)
+    ball_bounced = PingPongBall3(catch_x, catch_y, catch_z, v_after.x, v_after.y, v_after.z, ball.decay)
+
+    # check net pass
+    dt = math.fabs(catch_y / v_after.y)
+    h = catch_z + v_after.z * dt - 9.8 * 0.5 * dt ** 2
+    # 0.1525 + 0.02 = 0.1725
+    if h < 0.1725:
+        return False, None, None
+
+    # get status
+    x_final, y_final, z_final, v_final = DroppingStatus(ball_bounced)
+
+    # check out boundary
+    if math.fabs(x_final) >= 0.7625 or y_final > -0.02 or y_final < -1.35:
+        return False, None, None
+
+    return True, (x_final, y_final, z_final), v_final
+
+
+def correctBatPosition(v_ball, x, y, z, radius):
+    """
+    correct bat position
+    :param v_ball:
+    :param x:
+    :param y:
+    :param z:
+    :param radius:
+    :return: x,y,z
+    """
+    vec = v_ball.multiply(radius / v_ball.length)
+    return x + vec.x, y + vec.y, z + vec.z
 
 
 class vector3:
@@ -87,7 +200,7 @@ class PingPongBall3:
         self.v = vector3(vx, vy, vz)
         self.radius = 0.02
         self.trace = [[x], [y], [z]]
-        self.TableBouncePoint = []
+        self.TableBouncePoint = [[], []]
         self.decay = decay
 
     def CheckOutBoundary(self):
@@ -110,7 +223,8 @@ class PingPongBall3:
 
     def CheckBounce(self, target):
         d = Point2SurfaceDistance(self.x, self.y, self.z, target.x, target.y, target.z, target.theta, target.phi)
-        if d <= self.radius:
+        if d <= self.radius and math.sqrt(
+                (self.x - target.x) ** 2 + (self.y - target.y) ** 2 + (self.z - target.z) ** 2) <= 0.075:
             x, y, z = self.TryMove(1e-5)
             d_next = Point2SurfaceDistance(x, y, z, target.x, target.y, target.z, target.theta, target.phi)
             if d_next < d:
@@ -119,10 +233,11 @@ class PingPongBall3:
 
     def TableBounce(self):
         self.v.z = -self.v.z * math.sqrt(1 - self.decay)
-        self.TableBouncePoint.append(self.y)
+        self.TableBouncePoint[1].append(self.y)
+        self.TableBouncePoint[0].append(self.x)
 
     def Bounce(self, bat):
-        v_after = reflection(self.v, bat.theta, bat.phi)
+        v_after = reflection(self.v, bat.v, bat.theta, bat.phi, self.decay)
         self.v = v_after
 
     def TryMove(self, time):
@@ -157,7 +272,7 @@ class PingPongBat3:
 class PingPongMgr3:
     def __init__(self, ball):
         self.ball = ball
-        self.tick = 1e-4
+        self.tick = 1e-3
         self.bats = [None, None]
 
     def start(self):
@@ -165,6 +280,8 @@ class PingPongMgr3:
             self.ball.Move(self.tick)
             if self.ball.CheckTableBounce():
                 self.ball.TableBounce()
+                if len(self.ball.TableBouncePoint) == 2:
+                    break
                 if len(self.ball.TableBouncePoint) >= 2 and self.ball.TableBouncePoint[-1] * self.ball.TableBouncePoint[
                     -2] > 0:
                     break
@@ -190,22 +307,3 @@ class PingPongMgr3:
         ax.plot_surface(x, x * 0, z, color='y', alpha=0.1)
         plt.ylim((-1.5, 1.5))
         plt.xlim((-1.5, 1.5))
-
-
-if __name__ == '__main__':
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-
-    b = PingPongBall3(0, 0, 0.2, 0.5, 4, 0.5, 0.1)
-    p = InterceptPoint3(b)
-    d = DroppingPoint(b)
-    print(p)
-    # bat1 = PingPongBat3(p[0], p[1]+b.radius, 0, 0, 0, 0, math.pi / 2, math.pi / 2)
-    ax.scatter(p[0], p[1], p[2], color='r')
-    ax.scatter(d[0], d[1], 0.02, color='r')
-    mgr = PingPongMgr3(b)
-    # mgr.bats[1] = bat1
-    mgr.start()
-    mgr.show(ax)
-
-    plt.show()
